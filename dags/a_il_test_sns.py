a2b8a78adc9e7895bdcf3d751fed1c130e0d1492
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-08-26 18:17:44 +0800

    updated callback

--- dags/a_il_test_sns.py
+++ dags/a_il_test_sns.py
@@ -3,7 +3,6 @@ from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from textwrap import dedent
 from airflow.operators.email_operator import EmailOperator
 from airflow.providers.amazon.aws.notifications.sns import send_sns_notification
 
@@ -11,27 +10,27 @@ from airflow.providers.amazon.aws.notifications.sns import send_sns_notification
 sns_topic_arn = "arn:aws:sns:ap-east-1:695314914535:airflow-sns-tpoic"
 
 
-# dag_failure_sns_notification = send_sns_notification(
-#     aws_conn_id="aws_connection",
-#     region_name="ap-east-1",
-#     message="The DAG {{ dag.dag_id }} failed",
-#     target_arn=sns_topic_arn,
-# )
+dag_failure_sns_notification = send_sns_notification(
+    aws_conn_id="aws_connection",
+    region_name="ap-east-1",
+    message="The DAG {{ dag.dag_id }} failed",
+    target_arn=sns_topic_arn,
+)
 
 
 def task_failure_alert(context):
     print(f"Task has failed, task_instance_key_str:---------------------")
 
 
-def dag_check_SNS(context):
+# def dag_check_SNS(context):
 
-    sns = SnsPublishOperator(
-        task_id="check_list",
-        target_arn=sns_topic_arn,
-        subject=f"testing-subject{context['task_instance_key_str']}",
-        message="testing-content",
-    )
-    sns.execute(context)
+#     sns = SnsPublishOperator(
+#         task_id="check_list",
+#         target_arn=sns_topic_arn,
+#         subject=f"testing-subject{context['task_instance_key_str']}",
+#         message="testing-content",
+#     )
+#     sns.execute(context)
 
 
 with DAG(
@@ -52,7 +51,7 @@ with DAG(
     a_il_test_fail_5 = PythonOperator(
         task_id="test_sns_fallback",
         python_callable=lambda: 1 / 0,
-        on_failure_callback=dag_check_SNS
+        on_failure_callback=dag_failure_sns_notification
     )
 
     a_il_test_fail_5
