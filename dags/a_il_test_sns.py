b8f4d83232a2a7010074e1180d529f6def772c30
vaiivan <105779710+vaiivan@users.noreply.github.com>
2024-09-04 15:51:53 +0800

    updated sns with timestamp

--- dags/a_il_test_sns.py
+++ dags/a_il_test_sns.py
@@ -1,20 +1,17 @@
-import os 
 from airflow import DAG
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
-from textwrap import dedent
-from airflow.operators.email_operator import EmailOperator
+from datetime import datetime, timedelta, timezone
 from airflow.providers.amazon.aws.notifications.sns import send_sns_notification
 
 
 sns_topic_arn = "arn:aws:sns:ap-east-1:695314914535:airflow-sns-tpoic"
 
+time_now = datetime.now(timezone(timedelta(hours=8))).strftime('%Y%m%d %H:%M:%S')
 
 dag_failure_sns_notification = send_sns_notification(
     aws_conn_id="aws_connection",
     region_name="ap-east-1",
-    message="------- The DAG {{ dag.dag_id }} failed ---------123",
+    message=f"The DAG {{ dag.dag_id }} failed at {time_now}",
     target_arn=sns_topic_arn,
 )
 
@@ -24,12 +21,11 @@ with DAG(
     description="DAG testing for SES",
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    # on_failure_callback=task_failure_alert,
 ) as dag:
-    a_il_test_fail_5 = PythonOperator(
+    test_sns_fallback = PythonOperator(
         task_id="test_sns_fallback",
         python_callable=lambda: 1 / 0,
         on_failure_callback=dag_failure_sns_notification
     )
 
-    a_il_test_fail_5
+    test_sns_fallback
